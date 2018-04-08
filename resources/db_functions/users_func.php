53bdfe97d670dbc355071068b79c12614df4238d
VolodymyrShkykavyi <volodymyr.shkykavyi@gmail.com>
2018-04-08 13:42:36 +0300

    registration

--- resources/db_functions/users_func.php
+++ resources/db_functions/users_func.php
@@ -23,4 +23,28 @@ function is_admin($login){
     return(!empty($res));
 }
 
-var_dump(is_admin('admin'));
+function add_user($login, $email, $passw, $is_admin=0, $firstname="", $lastname="", $addres="", $img=""){
+    $login = trim($login);
+    $email = trim($email);
+    $passw = trim($passw);
+    if (!$login || !$email || !$passw)
+        return(false);
+    if ($is_admin != 1)
+        $is_admin = 0;
+    $conn = db_connect();
+    $login = mysqli_real_escape_string($conn, $login);
+    $email = mysqli_real_escape_string($conn, $email);
+    $passw = mysqli_real_escape_string($conn, $passw);
+    $firstname = mysqli_real_escape_string($conn, $firstname);
+    $lastname = mysqli_real_escape_string($conn, $lastname);
+    $addres = mysqli_real_escape_string($conn, $addres);
+    $img = mysqli_real_escape_string($conn, $img);
+    $res = db_query($conn, "SELECT * FROM `users` WHERE `login` = '{$login}' OR `email` = '{$email}'");
+    if (!empty($res))
+        return(false);
+    db_query(db_connect(), "INSERT INTO `users` (`login`, `email`, `password`, `is_admin`, `firstname`, `lastname`, `address`, `img`)
+ VALUES ('{$login}', '{$email}', '{$passw}', '{$is_admin}', '{$firstname}', '$lastname', '{$addres}', '{$img}');");
+    return(true);
+}
+
+//var_dump(add_user('user33', 'adm2@adm.uaw', 'pass'));
