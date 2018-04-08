1ea65ee9d5445d69924c75a466ec622585e4e721
VolodymyrShkykavyi <volodymyr.shkykavyi@gmail.com>
2018-04-08 21:33:47 +0300

    login

--- resources/header.php
+++ resources/header.php
@@ -1,3 +1,16 @@
+<?php
+session_start();
+require_once ("db_functions/users_func.php");
+
+if($_POST['log_in'] == "Login"){
+    if (auth($_POST['login'], $_POST['passwd'])){
+        if (($user = get_user_by_login($_POST['login']))){
+            $_SESSION['id'] = $user['id'];
+            $_SESSION['login'] = $user['login'];
+        }
+    }
+}
+?>
 <header>
 <div id="headerImage">
 	<a href="./index.php">
@@ -13,20 +26,29 @@
 		Create account
 	</div>
 </div>
-
-<div id=headerLogin>
-	<form id="headForm" action="" method="post">
-		<div id="loginOrCreation">
-			User login
-		</div>
-		Username: <input id="login" type="text" name="login" value="">
-		<br>
-		Password: <input id="password" type="password" name="passwd" value="">
-		<input id="button" type="submit" name="submit" value="Login">
-	</form>
-</div>
-	
-
+    <?php
+    if (!$_SESSION['id'] && !$_SESSION['login']) {
+        ?>
+        <div id=headerLogin>
+            <form id="headForm" action="index.php" method="post">
+                <div id="loginOrCreation">
+                    User login
+                </div>
+                Username: <input id="login" type="text" name="login" value="">
+                <br>
+                Password: <input id="password" type="password" name="passwd" value="">
+                <input id="button" type="submit" name="log_in" value="Login">
+            </form>
+        </div>
+        <?php
+    } else {
+        ?>
+        <div>
+            Username: <?=$_SESSION['login'];?>
+        </div>
+    <?php
+    }
+    ?>
 <div id="headerCreateAccHidden">
 	<div id="headerCreateAccCloser">
 
