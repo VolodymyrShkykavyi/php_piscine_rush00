81f1bdac6652b173d89eb4d9ae3f2685e21e75b1
VolodymyrShkykavyi <volodymyr.shkykavyi@gmail.com>
2018-04-08 14:19:37 +0300

    change, del user

--- resources/db_functions/change_product.php
+++ resources/db_functions/change_product.php
@@ -1,29 +1,36 @@
 <?php
 
+require_once ("db_query.php");
+require_once ("get_products.php");
+require_once ("add_product.php");
+require_once ("del_product.php");
+
 //TODO: categories change
 function change_product_by_id($id, $arr){
 	if (!is_numeric($id) || $id < 1 || empty($arr) || !get_product_by_id($id))
 		return(false);
 	$conn = db_connect();
 	$res_arr = array();
-	if ($arr['name'])
+	if (trim($arr['name']))
 		$res_arr['name'] = mysqli_real_escape_string($conn, trim($arr['name']));
-	if ($arr['img'])
+	if (trim($arr['img']))
 		$res_arr['img'] = mysqli_real_escape_string($conn, trim($arr['img']));
-	if ($arr['description'])
+	if (trim($arr['description']))
 		$res_arr['description'] = mysqli_real_escape_string($conn, trim($arr['description']));
-	if ($arr['price'])
+	if (trim($arr['price']))
 		$res_arr['price'] = mysqli_real_escape_string($conn, trim($arr['price']));
-	if ($arr['SKU'])
+	if (trim($arr['SKU']))
 		$res_arr['SKU'] = mysqli_real_escape_string($conn, trim($arr['SKU']));
-	$sql = "UPDATE `products` SET col1=val, col2=val ";
+	$sql = "UPDATE `products` SET ";
 	if (empty($res_arr))
 		return(false);
 	foreach ($res_arr as $key => $value){
-		$sql = $sql . " '{$key}'='{$value}', ";
+		$sql = $sql . " `{$key}`='{$value}', ";
 	}
 	$sql = trim($sql, ", ");
 	$sql = $sql . " WHERE `id` = '{$id}';";
 	db_query($conn, $sql);
 	return(true);
 }
+
+//var_dump(change_product_by_id(4, ['name' => 'dooogisdfi', 'SKU' => '12']));
--- resources/db_functions/users_func.php
+++ resources/db_functions/users_func.php
@@ -34,7 +34,7 @@ function add_user($login, $email, $passw, $is_admin=0, $firstname="", $lastname=
     $conn = db_connect();
     $login = mysqli_real_escape_string($conn, $login);
     $email = mysqli_real_escape_string($conn, $email);
-    $passw = mysqli_real_escape_string($conn, $passw);
+    $passw = mysqli_real_escape_string($conn,  hash('whirlpool', $passw));
     $firstname = mysqli_real_escape_string($conn, $firstname);
     $lastname = mysqli_real_escape_string($conn, $lastname);
     $addres = mysqli_real_escape_string($conn, $addres);
@@ -47,4 +47,78 @@ function add_user($login, $email, $passw, $is_admin=0, $firstname="", $lastname=
     return(true);
 }
 
+function get_user_by_id($id){
+    $id = trim($id);
+    if (!$id || !is_numeric($id) || $id < 0)
+        return(false);
+    $res = db_query(db_connect(), "SELECT * FROM `users` WHERE `id` = '{$id}' LIMIT 1");
+    if (empty($res))
+        return(false);
+    return($res[0]);
+}
+
+function get_user_by_login($login){
+    $login = trim($login);
+    if (!$login)
+        return(false);
+    $conn = db_connect();
+    $login = mysqli_real_escape_string($conn, $login);
+    $res = db_query($conn, "SELECT * FROM `users` WHERE `login` = '{$login}' LIMIT 1");
+    if (empty($res))
+        return(false);
+    return($res[0]);
+}
+
+function del_user_by_id($id){
+    if (!is_numeric($id))
+        return(false);
+    if (!get_user_by_id($id))
+        return(false);
+    db_query(db_connect(), "DELETE FROM `users` WHERE `id` = '{$id}';");
+    return(true);
+}
+
+function change_user_by_id($id, $arr){
+    $id = trim($id);
+    if (!$id || !get_user_by_id($id))
+        return(false);
+    $conn = db_connect();
+    $res_arr = array();
+    if (trim($arr['login']))
+        $res_arr['login'] = mysqli_real_escape_string($conn, trim($arr['login']));
+    if (trim($arr['email']))
+        $res_arr['email'] = mysqli_real_escape_string($conn, trim($arr['email']));
+    if (trim($arr['password']))
+        $res_arr['password'] = mysqli_real_escape_string($conn,  hash('whirlpool',trim($arr['password'])));
+    if (trim($arr['is_admin']))
+        $res_arr['is_admin'] = mysqli_real_escape_string($conn, trim($arr['is_admin']));
+    if (trim($arr['firstname']))
+        $res_arr['firstname'] = mysqli_real_escape_string($conn, trim($arr['firstname']));
+    if (trim($arr['lastname']))
+        $res_arr['lastname'] = mysqli_real_escape_string($conn, trim($arr['lastname']));
+    if (trim($arr['address']))
+        $res_arr['address'] = mysqli_real_escape_string($conn, trim($arr['address']));
+    if (trim($arr['img']))
+        $res_arr['img'] = mysqli_real_escape_string($conn, trim($arr['img']));
+    if (empty($res_arr))
+        return(false);
+
+    $sql = "UPDATE `users` SET ";
+    foreach ($res_arr as $key => $value){
+        $sql = $sql . " `{$key}`='{$value}', ";
+    }
+    $sql = trim($sql, ", ");
+    $sql = $sql . " WHERE `id` = '{$id}';";
+    db_query($conn, $sql);
+    return(true);
+}
+
 //var_dump(add_user('user33', 'adm2@adm.uaw', 'pass'));
+
+//var_dump(get_user_by_id(3));
+
+//var_dump(get_user_by_login('admin23'));
+
+//var_dump(del_user_by_id(14));
+
+//var_dump(change_user_by_id(3, ['login' => 'best_user', 'password' => 'new']));
